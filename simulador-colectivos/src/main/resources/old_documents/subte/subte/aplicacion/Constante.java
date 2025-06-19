63a2eae9c607c60687f728e88efd7c459c0fa268
“Miyen” <miyenbran@gmail.com>
2025-06-19 17:47:25 -0300

    workspace config

--- simulador-colectivos/src/main/resources/old_documents/subte/subte/aplicacion/Constante.java
+++
@@ -1,9 +0,0 @@
-package subte.aplicacion;
-
-public class Constante {
-
-	public static final int MAS_RAPIDO = 1;
-	public static final int MENOS_CONGESTION = 2;
-	public static final int MENOS_TRANSBORDO = 3;
-
-}
